9e7dbb147a8ca6fbe6018e0a25fde54b0fbc21b1
Marianna Foos <marianna.foos@gmail.com>
2013-08-16 10:12:38 -0400

    Now will create a seq length x seq length matrix of nuc position with function - am unsure how to make work with function, so removing

--- ConsensusSeq.py
+++ ConsensusSeq.py
@@ -1,8 +1,25 @@
 fasta = open("rosalind_cons.txt","r")
 
-dataset = []
+
+def makeProfile(seq):
+	aCount = [0]*(len(seq))
+	cCount = [0]*(len(seq))
+	gCount = [0]*(len(seq))
+	tCount = [0]*(len(seq))
+	for i in range(len(seq)):
+		if seq[i] == "A":
+			aCount[i] += 1
+		elif seq[i] == "C":
+			cCount[i] += 1
+		elif seq[i] == "G":
+			gCount[i] += 1
+		elif seq[i] == "T":
+			tCount[i] += 1
+	print aCount
+
 for line in fasta.readlines():
 	if line[0] != ">":
-		dataset.append(line)
+		line = line.strip()
+	#	print line, len(line)
+		makeProfile(line)
 
-print dataset[1]
