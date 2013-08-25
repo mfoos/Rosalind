53645072a7cd8a89adee2dd698152530b0d8d18f
Marianna Foos <marianna.foos@gmail.com>
2013-08-25 14:57:43 -0400

    most recent version of findSharedMotif before breaking it down again

--- findSharedMotif.py
+++ findSharedMotif.py
@@ -6,25 +6,30 @@ for entry in fasta:
 	if entry[0] == ">":
 		next
 	else:
-		seqSet.append(entry)
+		seqSet.append(entry.strip())
 
 subStringGenerator = seqSet.pop(0)
 #any longest common substring will exist in any sequence in the dataset
 
-shortestString = len(min(seqSet, key=len))
+end = size = len(min(seqSet, key=len))
 start = 0
-end = 5
-size = 5
-match = False
+newMin = None
 while start < len(subStringGenerator):
 	subString = subStringGenerator[start:end]
+	mismatch = False	
 	for seq in seqSet:
 		if subString in seq:
-			print subString,
-			match = True #there exists a common substring at this length
+			print subString
 		else:
-			print "subString absent"
+			print "String %s absent from this record" % subString
+			mismatch = True
 			break
+
+	if mismatch == False:
+		newMin = len(subString)		
+
 	start = end
 	end += size
+	end -= 1
 
+print newMin
